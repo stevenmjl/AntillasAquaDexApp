d3c750e41798630927b893fe51a0a33096250a3b
stevenmjl <stevenmjl.24@gmail.com>
2026-03-28 01:08:42 -0400

    Agrega EspecieRepository.kt

---
+++ app/src/main/java/edu/ucne/antillasaquadexapp/domain/repository/EspecieRepository.kt
@@ -0,0 +1,13 @@
+package edu.ucne.antillasaquadexapp.domain.repository
+
+import edu.ucne.antillasaquadexapp.domain.model.Especie
+import edu.ucne.antillasaquadexapp.util.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface EspecieRepository {
+    suspend fun getEspecies(page: Int = 1): Resource<List<Especie>>
+    suspend fun getEspecieById(id: Int): Resource<Especie>
+    fun getFavoritos(): Flow<List<Especie>>
+    suspend fun toggleFavorito(especieId: Int)
+    suspend fun esFavorito(especieId: Int): Boolean
+}
