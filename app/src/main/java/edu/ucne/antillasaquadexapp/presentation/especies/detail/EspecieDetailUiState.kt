94bbeb6723509d5dcf7d315b515b34924c8ec5fc
stevenmjl <stevenmjl.24@gmail.com>
2026-03-29 01:11:22 -0400

    Agrega EspecieDetailUiState.kt

---
+++ app/src/main/java/edu/ucne/antillasaquadexapp/presentation/especies/detail/EspecieDetailUiState.kt
@@ -0,0 +1,9 @@
+package edu.ucne.antillasaquadexapp.presentation.especies.detail
+
+import edu.ucne.antillasaquadexapp.domain.model.Especie
+
+data class EspecieDetailUiState(
+    val isLoading: Boolean = false,
+    val especie: Especie? = null,
+    val error: String? = null
+)
