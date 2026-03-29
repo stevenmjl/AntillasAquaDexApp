a2804f9e085e26f92236a7b4b769a7350c12f21a
stevenmjl <stevenmjl.24@gmail.com>
2026-03-29 01:36:24 -0400

    Agrega Screen.kt

---
+++ app/src/main/java/edu/ucne/antillasaquadexapp/presentation/navigation/Screen.kt
@@ -0,0 +1,34 @@
+package edu.ucne.antillasaquadexapp.presentation.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed interface Screen {
+    @Serializable
+    data object Bienvenida : Screen
+
+    @Serializable
+    data object Mapa : Screen
+
+    @Serializable
+    data class PaisDetalle(val paisNombre: String) : Screen
+
+    @Serializable
+    data class ZonaDetalle(
+        val zonaNombre: String,
+        val latitud: Double,
+        val longitud: Double,
+        val especieIds: List<Int>
+    ) : Screen
+
+    @Serializable
+    data object EspecieLista : Screen
+
+    @Serializable
+    data class EspecieDetalle(val especieId: Int) : Screen
+
+    @Serializable
+    data object Favoritos : Screen
+
+    @Serializable
+    data object Perfil : Screen
+}
