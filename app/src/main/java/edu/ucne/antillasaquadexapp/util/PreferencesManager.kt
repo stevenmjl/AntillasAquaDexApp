d8a3ece08194a188dc55c94fc9e8271f334c8334
stevenmjl <stevenmjl.24@gmail.com>
2026-04-04 00:09:23 -0400

    Agrega mapeo del volumen

--- app/src/main/java/edu/ucne/antillasaquadexapp/util/PreferencesManager.kt
+++ app/src/main/java/edu/ucne/antillasaquadexapp/util/PreferencesManager.kt
@@ -3,6 +3,7 @@ package edu.ucne.antillasaquadexapp.util
 import android.content.Context
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.floatPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
@@ -17,15 +18,27 @@ class PreferencesManager @Inject constructor(
     @ApplicationContext private val context: Context
 ) {
     private val IS_INITIAL_SYNC_COMPLETED = booleanPreferencesKey("is_initial_sync_completed")
+    private val MUSIC_VOLUME = floatPreferencesKey("music_volume")
 
     val isInitialSyncCompleted: Flow<Boolean> = context.dataStore.data
         .map { preferences ->
             preferences[IS_INITIAL_SYNC_COMPLETED] ?: false
         }
 
+    val musicVolume: Flow<Float> = context.dataStore.data
+        .map { preferences ->
+            preferences[MUSIC_VOLUME] ?: 0.7f
+        }
+
     suspend fun setInitialSyncCompleted(completed: Boolean) {
         context.dataStore.edit { preferences ->
             preferences[IS_INITIAL_SYNC_COMPLETED] = completed
+        }
+    }
+
+    suspend fun setMusicVolume(volume: Float) {
+        context.dataStore.edit { preferences ->
+            preferences[MUSIC_VOLUME] = volume
         }
     }
 }
