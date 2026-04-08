f8ed0a7b43609a39795e32bb025aad02d4d3ecfb
stevenmjl <stevenmjl.24@gmail.com>
2026-04-08 02:44:43 -0400

    Agrega TriviaRepository.kt

---
+++ app/src/main/java/edu/ucne/antillasaquadexapp/domain/repository/TriviaRepository.kt
@@ -0,0 +1,13 @@
+package edu.ucne.antillasaquadexapp.domain.repository
+
+import edu.ucne.antillasaquadexapp.data.local.entities.TriviaEntity
+import edu.ucne.antillasaquadexapp.data.local.model.PreguntaTrivia
+import kotlinx.coroutines.flow.Flow
+
+interface TriviaRepository {
+    fun getPreguntasPorCategoria(categoria: String): List<PreguntaTrivia>
+    fun getProgresoPorCategoria(categoria: String): Flow<TriviaEntity?>
+    suspend fun guardarProgreso(progreso: TriviaEntity)
+    suspend fun actualizarVidas(categoria: String, vidas: Int)
+    suspend fun registrarFallo(categoria: String, preguntaId: Int)
+}
