844a215f3fe1f6c74cceda4177b0a0fff474e141
stevenmjl <stevenmjl.24@gmail.com>
2026-04-08 02:44:05 -0400

    Agrega el nuevo Dao al AppDatabase.kt

--- app/src/main/java/edu/ucne/antillasaquadexapp/data/local/database/AppDatabase.kt
+++ app/src/main/java/edu/ucne/antillasaquadexapp/data/local/database/AppDatabase.kt
@@ -5,21 +5,25 @@ import androidx.room.RoomDatabase
 import edu.ucne.antillasaquadexapp.data.local.dao.FavoritoDao
 import edu.ucne.antillasaquadexapp.data.local.dao.EspecieDao
 import edu.ucne.antillasaquadexapp.data.local.dao.UsuarioDao
+import edu.ucne.antillasaquadexapp.data.local.dao.TriviaDao
 import edu.ucne.antillasaquadexapp.data.local.entities.FavoritoEntity
 import edu.ucne.antillasaquadexapp.data.local.entities.EspecieEntity
 import edu.ucne.antillasaquadexapp.data.local.entities.UsuarioEntity
+import edu.ucne.antillasaquadexapp.data.local.entities.TriviaEntity
 
 @Database(
     entities = [
         UsuarioEntity::class,
         FavoritoEntity::class,
-        EspecieEntity::class
+        EspecieEntity::class,
+        TriviaEntity::class
     ],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun usuarioDao(): UsuarioDao
     abstract fun favoritoDao(): FavoritoDao
     abstract fun especieDao(): EspecieDao
+    abstract fun triviaDao(): TriviaDao
 }
