dced64c9d1853d0ac5a6a3e32833eb8613fb9ade
stevenmjl <stevenmjl.24@gmail.com>
2026-04-08 02:43:28 -0400

    Agrega TriviaDao.kt

---
+++ app/src/main/java/edu/ucne/antillasaquadexapp/data/local/dao/TriviaDao.kt
@@ -0,0 +1,27 @@
+package edu.ucne.antillasaquadexapp.data.local.dao
+
+import androidx.room.*
+import edu.ucne.antillasaquadexapp.data.local.entities.TriviaEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TriviaDao {
+
+    @Query("SELECT * FROM trivia_progreso WHERE categoria = :categoria")
+    fun getProgresoPorCategoria(categoria: String): Flow<TriviaEntity?>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveProgreso(progreso: TriviaEntity)
+
+    @Query("UPDATE trivia_progreso SET ultimaPreguntaId = :preguntaId WHERE categoria = :categoria")
+    suspend fun updateUltimaPregunta(categoria: String, preguntaId: Int)
+
+    @Query("UPDATE trivia_progreso SET vidasRestantes = :vidas WHERE categoria = :categoria")
+    suspend fun updateVidas(categoria: String, vidas: Int)
+
+    @Query("UPDATE trivia_progreso SET falloEnPreguntaId = :preguntaId WHERE categoria = :categoria")
+    suspend fun registrarFallo(categoria: String, preguntaId: Int)
+    
+    @Query("SELECT * FROM trivia_progreso")
+    fun getAllProgreso(): Flow<List<TriviaEntity>>
+}
